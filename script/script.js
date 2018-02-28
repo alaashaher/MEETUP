95ee963d92974d61dd947e13b952675d22adfd06
الاء شاهر <30909733+alaashaher@users.noreply.github.com>
2018-02-28 20:36:40 +0200

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,26 @@
+function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(51.5, -0.12),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    }
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
+
+function myFunction() {
+        var email;
+
+        email = document.getElementById("textEmail").value;
+
+            var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+            if (reg.test(textEmail.value) == false) {
+              alert("invalid email");
+              return false;
+            } 
+       return true;
+}
+
+function clearContents(element) {
+  element.value = '';
+}
