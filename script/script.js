5bf94e61954c9543ff30c80dc1644824af98ab74
الاء شاهر <30909733+alaashaher@users.noreply.github.com>
2018-03-04 00:33:01 +0200

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -24,3 +24,8 @@ function myFunction() {
 function clearContents(element) {
   element.value = '';
 }
+
+var remove_element = document.getElementById("remove_calss");
+if(navigator.userAgent.indexOf("Firefox") != -1 ) {
+         remove_element.classList.remove("time3");
+}
